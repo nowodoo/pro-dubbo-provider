1ed7d04fae506c73604e15e8c767b58e91728767
zach ma <zach.ma@centling.com>
2016-10-20 20:28:37 +0800

    03 server端调试成功。

--- src/main/java/com/zach/dubbo/service/ServerMain.java
+++ src/main/java/com/zach/dubbo/service/ServerMain.java
@@ -2,13 +2,11 @@ package com.zach.dubbo.service;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.IOException;
-
 /**
  * Created by Administrator on 2016-10-16.
  */
 public class ServerMain {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         //因为这个spring是dubbo自带的，所以在这里就直接使用就好了，不用在引入spring了
         final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath*:dubbo-provider.xml");
         context.start();
